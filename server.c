67794d6887d154f2c1dd766e62eb9be00561b884
Marisa Liu <43096612+marisaliu@users.noreply.github.com>
2018-11-18 15:15:33 -0500

    Update server.c

--- server.c
+++ server.c
@@ -3,6 +3,13 @@
  */
 /* $begin echoservertmain */
 #include "csapp.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "deck.h"
+#include "player.h"
+#include <ctype.h>
 
 void *thread(void *vargp);
 
