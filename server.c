04c94b945f106ad50be9bb15d0f3582920dc7e61
dylanb271 <dylanb271@gmail.com>
2018-11-19 12:31:14 -0500

    started on server sending/receiving

--- server.c
+++ server.c
@@ -12,7 +12,7 @@
 #include <ctype.h>
 
 void *thread(void *vargp);
-
+/*
 void echo(int connfd)
 {
     int n;
@@ -25,39 +25,41 @@ void echo(int connfd)
         Rio_writen(connfd, buf, n);
     }
 }
-
+*/
+char * serialize_char(char *buf, char val);
+char * serialize_card(char *buf, struct card *val);
+char * serialize_hand(char *buf, struct hand *val);
+ 
 int main(int argc, char **argv) 
 {
-    int listenfd, *connfdp;
+    char listenfd, *connfdp, buf[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
     pthread_t tid; 
+    char bookAdded;
+    char inputRank;
+    int transferCards;
+    int play = 1; //1 true, 0 false
+    int turn = 1; //1 true, 0 false
+    int win = 0;  //1 true, 0 false
+    int playAgain;
+    struct card* nextCard;
+    struct hand* temp;
+    user.book[0] = '\0'; 
+    computer.book[0] = '\0';
 
+/////////////////////////Set up ports///////////////////////
     if (argc != 2) {
 	fprintf(stderr, "usage: %s <port>\n", argv[0]);
 	exit(0);
     }
-    listenfd = Open_listenfd(argv[1]);
+    listenfd = Open_listenfd(argv[1]);  
 
-    while (1) {
-        clientlen=sizeof(struct sockaddr_storage);
-	connfdp = Malloc(sizeof(int)); //line:conc:echoservert:beginmalloc
-	*connfdp = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:conc:echoservert:endmalloc
-	Pthread_create(&tid, NULL, thread, connfdp);
-    }
-	/*
-	char bookAdded;
-  char inputRank;
-  int transferCards;
-  int play = 1; //1 true, 0 false
-  int turn = 1; //1 true, 0 false
-  int win = 0;  //1 true, 0 false
-  int playAgain;
-  struct card* nextCard;
-  struct hand* temp;
-  user.book[0] = '\0'; 
-  computer.book[0] = '\0';
-
+    clientlen=sizeof(struct sockaddr_storage);
+    connfdp = Malloc(sizeof(int)); //line:conc:echoservert:beginmalloc
+    *connfdp = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:conc:echoservert:endmalloc
+  //    Pthread_create(&tid, NULL, thread, connfdp);
+  
 //////////////////////Starts the game/////////////////////////
   while(play==1){                              //start game
     playAgain = 0;
@@ -68,11 +70,12 @@ int main(int argc, char **argv)
     computer.hand_size = 7;
 ///////////////////////Loops Through Players////////////////////////////////   
     while(win == 0){
-
-/////////////////////Player 1's Turn///////////////////////////////////
+////////////////////Player 1's Turn///////////////////////////////////
       while((turn==1) && (win == 0)){
-        display_hand(&user);                          //Display player 1's hand
-        display_book(&user,1);                        //Display player 1's book 
+	//need to figure out how large a hand is to tell how many bytes to send
+        //send(connfdp, serialize_hand(buf, user->card_list, _______, 0);
+
+	display_book(&user,1);                        //Display player 1's book 
         display_book(&computer,2);                    //Display user 1's book
         if(user.hand_size == 0){                      //If player's hand is empty, player will draw a card and end their turn
 	  nextCard = next_card();
@@ -81,9 +84,14 @@ int main(int argc, char **argv)
 	  printf("\n  - Go Fish, Player 1 draws %c%c,", nextCard->rank, nextCard->suit);
 	  turn = 0;
 	} 
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////HANDLE CLIENT TO SERVER INTERACTION HERE///////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 	else{
-	  inputRank = user_play(&user);                 //Prompt player 1 to enter a rank
-	  temp = copy_hand_list(&user);
+	  if((inputRank = recv(connfdp, buf, 1, 0)) < 0){ //receive a message of 1 char
+	    printf("\nERROR RECEIVING FROM CLIENT");
+	  } 
+          temp = copy_hand_list(&user);
           transferCards = search(&computer, inputRank); //Check player 2's hand to see if they have that rank
      /////////////If they have the rank transfer the cards///////////////////////////////////  
 	  if(transferCards == 1){          
@@ -211,17 +219,36 @@ int main(int argc, char **argv)
         printf("\nError! Please enter Y or N");
       }
     }
-  }*/
+  }
 }
 
 /* Thread routine */
 void *thread(void *vargp) 
 {  
-    int connfd = *((int *)vargp);
+    char connfd = *((char *)vargp);
     Pthread_detach(pthread_self()); //line:conc:echoservert:detach
     Free(vargp);                    //line:conc:echoservert:free
-    echo(connfd);
+    inputRank = connfd;
     Close(connfd);
     return NULL;
 }
 /* $end echoservertmain */
+
+char * serialize_char(char *buf, char val){
+  buf[0] = val;
+  return buf+sizeof(char);
+}
+
+char * serialize_card(char *buf, struct card *val){
+//not sure if these indices have correct spacing
+  buf[0] = serialize_char(buf, val->suit);
+  buf[1] = serialize_char(buf, val->rank);
+  return buf+sizeof(struct card);
+}
+
+char * serialize_hand(char *buf, struct hand *val){
+  buf[0] = serialize_card(buf, val->top);
+  buf[1] = //help
+  return buf+sizeof(struct hand);
+}
+ 
