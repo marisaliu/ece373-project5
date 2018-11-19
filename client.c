5e5fb38eae3c6ccb1583b7933321baa536ab2c55
root <myliu@umass.edu>
2018-11-19 09:46:27 -0500

    client input

--- client.c
+++ client.c
@@ -1,3 +1,12 @@
+#include "csapp.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "deck.h"
+#include "player.h"
+#include <ctype.h>
+
 /*
  * echoclient.c - An echo client
  */
@@ -9,6 +18,7 @@ int main(int argc, char **argv)
     int clientfd;
     char *host, *port, buf[MAXLINE];
     rio_t rio;
+		int play = 1;
 
     if (argc != 3) {
 	fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
@@ -20,10 +30,15 @@ int main(int argc, char **argv)
     clientfd = Open_clientfd(host, port);
     Rio_readinitb(&rio, clientfd);
 
-    while (Fgets(buf, MAXLINE, stdin) != NULL) {
-	Rio_writen(clientfd, buf, strlen(buf));
-	Rio_readlineb(&rio, buf, MAXLINE);
-	Fputs(buf, stdout);
+		//while play is true prompt the user for a rank and then display his hand and the books
+    while (play) {
+			buf = user_play(&user);
+			Rio_writen(clientfd, buf, strlen(buf)); //writes/sends it to server
+	//		Rio_readlineb(&rio, buf, MAXLINE);  //reads in from server
+			display_hand(&user);
+			display_book(&user,1);
+			display_book(&computer,2);
+			//	Fputs(buf, stdout);
     }
     Close(clientfd); //line:netp:echoclient:close
     exit(0);
