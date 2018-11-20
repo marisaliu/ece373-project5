5d3a027d066b073f859c2baff69ea073f7e1a432
ece373 <myliu@umass.edu>
2018-11-20 09:24:36 -0500

    set up client

--- client.c
+++ client.c
@@ -3,8 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "deck.h"
-#include "player.h"
+#include "card.h"
 #include <ctype.h>
 
 /*
@@ -28,19 +27,43 @@ int main(int argc, char **argv)
     port = argv[2];
 
     clientfd = Open_clientfd(host, port);
-    Rio_readinitb(&rio, clientfd);
+    struct hand* hand = Rio_readinitb(&rio, clientfd);
 
 		//while play is true prompt the user for a rank and then display his hand and the books
     while (play) {
-			buf = user_play(&user);
+			buf = user_play(&hand);
 			Rio_writen(clientfd, buf, strlen(buf)); //writes/sends it to server
-	//		Rio_readlineb(&rio, buf, MAXLINE);  //reads in from server
-			display_hand(&user);
-			display_book(&user,1);
-			display_book(&computer,2);
-			//	Fputs(buf, stdout);
+			Rio_readlineb(&rio, buf, MAXLINE);  //reads in from server
+			display_hand(&hand);
+			Fputs(buf, stdout);
     }
     Close(clientfd); //line:netp:echoclient:close
     exit(0);
 }
-/* $end echoclientmain */
+
+//////////////////////////////////////////////////////////////////
+///////////////////Prompts for User Input/////////////////////////
+/////////////////////////////////////////////////////////////////
+char user_play(struct hand* temp)
+{ 
+  char input;
+  int boolInHand = 0;
+  while(boolInHand == 0)
+  {
+    printf("\nPlayer 1's turn, enter a Rank: ");
+    scanf("%c", &input);
+    while((getchar()) != '\n');
+    while(temp != NULL)
+    {
+      if(temp->top.rank== toupper(input))
+      { 
+        boolInHand = 1;
+        return toupper(input);
+      }
+      temp = temp->next;
+    }
+    printf("Error - must have at least one card from rank to play");
+  }
+}
+
+
